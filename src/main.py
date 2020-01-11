adbe04ee9904ba125c82f5a68c374ba95ee27439
SaiChaitanyaBolla <SaiChaitanya.Bolla@gmail.com>
2020-01-11 19:52:40 +0530

    Feature 1 complete

---
+++ src/main.py
@@ -0,0 +1,10 @@
+sprint 1 started >> src/main.py
+git add .
+git commit -m Feature 1 complete
+echo Sprint 1 test started testing >> test/main.py
+git add .
+git commit - m Feature 2 complete
+git log
+
+
+echo sprint 1 started
